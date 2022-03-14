3ebafb61159ce0e20a91b10246b83eee26d21d77
Ankur <ext-ankur.bajpai@sok.fi>
2022-03-14 10:38:01 +0530

    Fixes: Unwanted code

--- js/script.js
+++ js/script.js
@@ -8,7 +8,3 @@ document.getElementById("hamburger").onclick = function toggleMenu() {
 document.getElementById("idBtnResume").onclick = function openResume() {
   window.open("https://drive.google.com/file/d/1Q8RUvXqnWgZK7QY1AtuMZ8oLy14MNhRb/view", "_blank");
 };
-
-document.getElementById("idBtnSAPUI5").onclick = function openSAPUI5() {
-  window.open("https://sapui5.hana.ondemand.com/", "_blank");
-};
