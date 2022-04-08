77b23e8c362b72c75151fd8050029d776854e060
Ankur <ext-ankur.bajpai@sok.fi>
2022-04-08 20:01:07 +0530

    Update resume

--- js/script.js
+++ js/script.js
@@ -6,5 +6,5 @@ document.getElementById("hamburger").onclick = function toggleMenu() {
 };
 
 document.getElementById("idBtnResume").onclick = function openResume() {
-  window.open("https://drive.google.com/file/d/1CEfU2u0v1Deeckvmhn9fLcL0a80rSgqm/view", "_blank");
+  window.open("https://drive.google.com/file/d/1roIlULW0GmSRqJ-dxQ_qW3StUXV8F7jN/view", "_blank");
 };
